51029eab59435314d8f94912dbe5593e86e0f6ab
Ocsael Neftali Ramirez Castillo <36492776+Ocsa@users.noreply.github.com>
2022-03-01 21:00:08 -0600

    Metodo contrasena y usuario(codigo)
    Se implemento el metodo para obtener  la contrasena y se obtiene y verifica codigo ingresado. Debe ser mejorado segun la practica pero el flujo es el mismo.

--- Proyecto/Proyecto.ino
+++ Proyecto/Proyecto.ino
@@ -8,6 +8,7 @@
 const byte FILAS = 4;     // Filas
 const byte COLUMNAS = 4;  // Columnas
 
+int estadoIngre=0;
 // Matriz de las teclas
 char teclas[FILAS][COLUMNAS] = {
     {'1', '2', '3','A'}, {'4', '5', '6','B'}, {'7', '8', '9','C'}, {'*', '0', '#','D'}};
@@ -22,6 +23,7 @@ Keypad teclado = Keypad(makeKeymap(teclas), filaPines, columnaPines, FILAS, COLU
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 String contrasena = "";
+String usuTempo ="";
 int digitosIngresados = 0;
 
 void setup() {
@@ -47,27 +49,54 @@ void seguridad() {
     char tecla = teclado.getKey();
     
     //Serial.print(tecla);
-    
+    //Verificamos el estado de ingreso si estamos en usuario o en contra
+    // usuario ---- ingreso ===0
+    // contra ---- ingreso ===1
     if (tecla) {
-        
+        if(estadoIngre==0){
         if(contrasena.length()<1){
           lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Usuario");
+            lcd.setCursor(0, 1);
         }
         if(tecla!='*'){
         contrasena += tecla;
         lcd.print(tecla);
         digitosIngresados++;
+        delay(15);
+        }else{
+          usuTempo = contrasena;
+        contrasena="";
+        estadoIngre=1;
+        delay(15);
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Codigo");
+          lcd.setCursor(0, 1);
+        
         }
-    }
-
-     if (tecla == '*') {
-        if (clave == contrasena) {  // correcto
+        }else{
+          if(contrasena.length()<1){
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Codigo");
+            lcd.setCursor(0, 1);
+        }
+        if(tecla!='*'){
+        contrasena += tecla;
+        lcd.print('*');
+        digitosIngresados++;
+        delay(15);
+        estadoIngre=3;
+        }else{
+          if (clave == contrasena) {  // correcto
             contrasena="";
             lcd.clear();
             lcd.setCursor(0, 0);
             lcd.print("BIENVENIDO");
             lcd.setCursor(0, 1);
-            lcd.print("A CASA");
+            lcd.print("AL PARQUEO");
             contrasena="";
             delay(200);
            
@@ -82,15 +111,21 @@ void seguridad() {
         }
         contrasena = "";
         digitosIngresados = 0;
+        estadoIngre=0;
         delay(100);
-        
-        //lcd.clear();
-        
-    }else if(tecla=='#'){ 
+        }
+          
+        }
+    }
+
+     if(tecla=='#'){ 
       lcd.clear();
       lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
       lcd.print("CASA ACYE1");
       lcd.setCursor(2,1);   //Move cursor to character 2 on line 1
       lcd.print("A-G4-S2");
+      estadoIngre=0;
+      contrasena ="";
+      delay(20);
     }
 }
