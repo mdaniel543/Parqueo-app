34bfa5f936868962f84db82e4045a34e75932d21
Ocsael Neftali Ramirez Castillo <36492776+Ocsa@users.noreply.github.com>
2022-03-01 19:11:32 -0600

    Contrasena con teclado Normal
    Se crea la contrasena con teclado normal

--- Proyecto/Proyecto.ino
+++ Proyecto/Proyecto.ino
@@ -1,7 +1,28 @@
 #include <LiquidCrystal.h>
+#include <Key.h>
+#include <Keypad.h>
 
+//PRUEBA
+
+// Configuración del teclado
+const byte FILAS = 4;     // Filas
+const byte COLUMNAS = 3;  // Columnas
+
+// Matriz de las teclas
+char teclas[FILAS][COLUMNAS] = {
+    {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}};
+
+// Asignación de las teclas a los pines de arduino
+byte filaPines[FILAS] = {25, 26, 27, 28};
+byte columnaPines[COLUMNAS] = {22, 23, 24};
+String clave = "202104";
+// Instancia teclado
+Keypad teclado =
+Keypad(makeKeymap(teclas), filaPines, columnaPines, FILAS, COLUMNAS);
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+String contrasena = "";
+int digitosIngresados = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,13 +30,67 @@ void setup() {
   lcd.clear();
   lcd.home();
   lcd.setCursor(0, 0);
-  lcd.print("HOLA");
+  lcd.print("Bienvenido");
   lcd.setCursor(0, 1);
-  lcd.print("Iniciando...");
+  lcd.print("Grupo5-SeccionA");
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   delay(10);
+  seguridad();
+}
+void seguridad() {
+  
+  //lcd.clear();
+    char tecla = teclado.getKey();
+    
+    //Serial.print(tecla);
+    
+    if (tecla) {
+        
+        if(contrasena.length()<1){
+          lcd.clear();
+        }
+        if(tecla!='*'){
+        contrasena += tecla;
+        lcd.print("*");
+        digitosIngresados++;
+        }
+    }
+
+     if (tecla == '*') {
+        if (clave == contrasena) {  // correcto
+            contrasena="";
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("BIENVENIDO");
+            lcd.setCursor(0, 1);
+            lcd.print("A CASA");
+            contrasena="";
+            delay(200);
+           
+            //etapa2 = 1;
+        } else {  // incorrecto
+            contrasena="";
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("ERROR EN Contrasena");
+            lcd.setCursor(2,1); 
+            contrasena="";
+        }
+        contrasena = "";
+        digitosIngresados = 0;
+        delay(100);
+        
+        //lcd.clear();
+        
+    }else if(tecla=='#'){ 
+      lcd.clear();
+      lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
+      lcd.print("CASA ACYE1");
+      lcd.setCursor(2,1);   //Move cursor to character 2 on line 1
+      lcd.print("A-G4-S2");
+    }
 }
